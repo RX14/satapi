3105a9d1c5ba89ab7980efd3e4b4eca8adae2781
RX14 <chris@rx14.co.uk>
2014-10-25 19:35:40 +0100

    Comment out pending tests

--- spec/api/controllers/tle_controller_spec.rb
+++ spec/api/controllers/tle_controller_spec.rb
@@ -1,13 +1,13 @@
-require "spec_helper"
-
-RSpec.describe "/tle" do
-    pending "add some examples to #{__FILE__}" do
-        before do
-            get "/tle"
-        end
-
-        it "returns hello world" do
-            expect(last_response.body).to eq "Hello World"
-        end
-    end
-end
+# require "spec_helper"
+#
+# RSpec.describe "/tle" do
+#     pending "add some examples to #{__FILE__}" do
+#         before do
+#             get "/tle"
+#         end
+#
+#         it "returns hello world" do
+#             expect(last_response.body).to eq "Hello World"
+#         end
+#     end
+# end
