1d7f1d79b1423e6163973870017d6f3bbbd4ec31
RX14 <chris@rx14.co.uk>
2014-10-25 23:44:17 +0100

    Add post_json function

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,3 +31,7 @@ def app(app = nil, &blk)
     @app ||= block_given? ? app.instance_eval(&blk) : app
     @app ||= Padrino.application
 end
+
+def post_json(uri, json)
+    post(uri, JSON.generate(json), "CONTENT_TYPE" => "application/json")
+end
