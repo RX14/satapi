d057a0692044c0fc0cf226ac3aede4c4d2928867
RX14 <chris@rx14.co.uk>
2014-10-26 01:57:07 +0100

    Make json_helper conform to specs

--- api/helpers/json_helper.rb
+++ api/helpers/json_helper.rb
@@ -1,22 +1,33 @@
 module Satapi
     module JSONHelper
-        def parse_json(request)
-            return nil if request_null? request
+        def parse_json(req)
+            body = extract_body(req)
 
-            request.body.rewind
             begin
-                JSON.parse(request.body.read)
+                JSON.parse(body)
             rescue JSON::ParserError
                 nil
             end
         end
 
-        def request_null?(req)
-            if req.body.class == Puma::NullIO
-                true
-            elsif req.body.size <= 0
-                true
+        def extract_body(req)
+            return nil if request_body_empty? req
+
+            if req.body.respond_to? :read
+                req.body.rewind if req.body.respond_to? :rewind
+                req.body.read
+            else
+                req.body
             end
+        end
+
+        def request_body_empty?(req)
+            return true if req.nil?
+            return true if req.body.nil?
+            return true if req.body.class == Puma::NullIO
+            return true if req.body.size <= 0
+
+            false
         end
     end
     class Api
