3d09a439bbd58d375efb42c9a5ca87dfcbbb8620
RX14 <chris@rx14.co.uk>
2014-10-27 15:56:42 +0000

    Change formatting to match the test

--- api/controllers/tle.rb
+++ api/controllers/tle.rb
@@ -17,30 +17,32 @@ Satapi::Api.controllers :tle do
         tle = uk.me.g4dpz.satellite.TLE.new(tle_lines.to_java(:String))
 
         response = {}
-        response[:catnum] = tle.getCatnum
-        response[:name] = tle.getName
-        response[:setnum] = tle.getSetnum
-        response[:year] = tle.getYear
-        response[:refepoch] = tle.getRefepoch
-        response[:inclination] = tle.getIncl
-        response[:raan] = tle.getRaan
+        # rubocop:disable Style/SpaceInsideBrackets
+        response[:catnum      ] = tle.getCatnum
+        response[:name        ] = tle.getName
+        response[:setnum      ] = tle.getSetnum
+        response[:year        ] = tle.getYear
+        response[:refepoch    ] = tle.getRefepoch
+        response[:inclination ] = tle.getIncl
+        response[:raan        ] = tle.getRaan
         response[:eccentricity] = tle.getEccn
-        response[:argper] = tle.getArgper
+        response[:argper      ] = tle.getArgper
         response[:mean_anomoly] = tle.getMeanan
-        response[:mean_motion] = tle.getMeanmo
-        response[:drag] = tle.getDrag
-        response[:nddot6] = tle.getNddot6
-        response[:bstar] = tle.getBstar
-        response[:orbitnum] = tle.getOrbitnum
-        response[:epoch] = tle.getEpoch
-        response[:xndt2o] = tle.getXndt2o
-        response[:xincl] = tle.getXincl
-        response[:xnodeo] = tle.getXnodeo
-        response[:eo] = tle.getEo
-        response[:omegao] = tle.getOmegao
-        response[:xmo] = tle.getXmo
-        response[:xno] = tle.getXno
-        response[:deepspace] = tle.isDeepspace
+        response[:mean_motion ] = tle.getMeanmo
+        response[:drag        ] = tle.getDrag
+        response[:nddot6      ] = tle.getNddot6
+        response[:bstar       ] = tle.getBstar
+        response[:orbitnum    ] = tle.getOrbitnum
+        response[:epoch       ] = tle.getEpoch
+        response[:xndt2o      ] = tle.getXndt2o
+        response[:xincl       ] = tle.getXincl
+        response[:xnodeo      ] = tle.getXnodeo
+        response[:eo          ] = tle.getEo
+        response[:omegao      ] = tle.getOmegao
+        response[:xmo         ] = tle.getXmo
+        response[:xno         ] = tle.getXno
+        response[:deepspace   ] = tle.isDeepspace
+        # rubocop:enable Style/SpaceInsideBrackets
 
         JSON.pretty_generate(response) + "\n"
     end
