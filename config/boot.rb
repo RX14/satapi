85927e3ddacd0ffa20c35554c60bbde679babb8d
RX14 <chris@rx14.co.uk>
2014-10-28 09:10:07 +0000

    Fix comment wording

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
-# Defines our constants
+# Define our constants
 RACK_ENV = ENV["RACK_ENV"] ||= "development"  unless defined?(RACK_ENV)
 PADRINO_ROOT = File.expand_path("../..", __FILE__) unless defined?(PADRINO_ROOT)
 
