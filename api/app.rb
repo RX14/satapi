b158d3b3ed32046df2f3010ebe4ccdb8a615e3ca
RX14 <chris@rx14.co.uk>
2014-10-28 09:10:37 +0000

    Add RequestValidation middleware

--- api/app.rb
+++ api/app.rb
@@ -2,6 +2,10 @@ module Satapi
     class Api < Padrino::Application
         register Padrino::Helpers
 
+        use Committee::Middleware::RequestValidation,
+            schema: JSON.parse(
+                File.read("#{PADRINO_ROOT}/docs/schema/schema.json"))
+
         set :protect_from_csrf, false
     end
 end
